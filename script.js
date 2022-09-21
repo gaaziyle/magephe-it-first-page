ea31a94324175ab2e8545dd376e7503ea28ff396
Enes EFE <enes_efe_158@hotmail.com>
2022-09-21 20:05:13 +0300

    magephe-it-first-page

---
+++ script.js
@@ -0,0 +1,83 @@
+window.sr = new ScrollReveal()
+
+
+sr.reveal('#img1', {
+    delay: 200,
+});
+sr.reveal('#img2', {
+    delay: 400,
+});
+sr.reveal('#img3', {
+    delay: 600,
+});
+sr.reveal('#img4', {
+    delay: 800,
+});
+sr.reveal('#img5', {
+    delay: 1000,
+});
+sr.reveal('#img6', {
+    delay: 1200,
+});
+sr.reveal('#img7', {
+    delay:1400,
+});
+sr.reveal('#img8', {
+    delay: 1600,
+});
+sr.reveal('#img9', {
+    delay: 1800,
+});
+sr.reveal('#img10', {
+    delay: 2000,
+});
+sr.reveal('#img11', {
+    delay: 2200,
+});
+sr.reveal('#img12', {
+    delay: 2400,
+});
+sr.reveal('#img13', {
+    delay: 2600,
+});
+sr.reveal('#img14', {
+    delay: 2800,
+});
+sr.reveal('.point1', {
+    delay: 300,
+});
+sr.reveal('.point2', {
+    delay: 600,
+});
+sr.reveal('.point3', {
+    delay: 900,
+});
+sr.reveal('.point4', {
+    delay: 1200,
+});
+sr.reveal('.point5', {
+    delay: 1500,
+});
+sr.reveal('.point6', {
+    delay: 1800,
+});
+sr.reveal('.point7', {
+    delay: 2100,
+});
+sr.reveal('.point8', {
+    delay: 2400,
+});
+sr.reveal('.point9', {
+    delay: 2700,
+});
+sr.reveal('.point10', {
+    delay: 3000,
+});
+sr.reveal('.point11', {
+    delay: 3300,
+});
+sr.reveal('footer', {
+    delay: 3300,
+    origin: 'bottom',
+    distance: "50px"
+});
